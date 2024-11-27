548ec4f2dab8b2395c08cdcdab9d2f6c2ec3ef9a
MZiO <mziodev@gmail.com>
2024-11-27 17:12:48 +0100

    [Feature] Add solution to LeetCode 27 Remove Element problem.

--- 27_RemoveElement.playground/Contents.swift
+++ 27_RemoveElement.playground/Contents.swift
@@ -2,12 +2,27 @@ import Foundation
 
 class Solution {
     static func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        nums = nums.filter { $0 != val }
+        let numsCount = nums.count
+        var index = 0
+        var valCount = 0
         
-        return nums.count
+        while index < numsCount - valCount {
+            if nums[index] == val {
+                nums.remove(at: index)
+                nums.append(0)
+                
+                valCount += 1
+            } else {
+                index += 1
+            }
+            
+        }
+        
+        return numsCount - valCount
     }
 }
 
 var nums1 = [3,2,2,3]
 
 Solution.removeElement(&nums1, 3)
+print(nums1)
